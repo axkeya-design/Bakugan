4e03a25684314d71ed4020533663c3db20428731
Axkeya <axkeya@gmail.com>
2026-07-12 22:17:41 +0500

    Вывод всех бакуганов из списка

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum {
     PYRUS,
@@ -7,7 +8,8 @@ typedef enum {
     SUBTERRA,
     HAOS,
     VENTUS,
-    DARKUS
+    DARKUS,
+	UNKNOWN_ELEMENT
 } Element;
 
 const char *ELEMENT_NAMES[] = {
@@ -16,24 +18,35 @@ const char *ELEMENT_NAMES[] = {
     "Subterra",
     "Haos",
     "Ventus",
-    "Darkus"
+    "Darkus",
+	"Unknown"
 };
 
+Element string_to_element(const char *str) {
+    if (strcmp(str, "PYRUS") == 0) return PYRUS;
+    if (strcmp(str, "AQUOS") == 0) return AQUOS;
+    if (strcmp(str, "SUBTERRA") == 0) return SUBTERRA;
+    if (strcmp(str, "HAOS") == 0) return HAOS;
+    if (strcmp(str, "VENTUS") == 0) return VENTUS;
+	if (strcmp(str, "DARKUS") == 0) return DARKUS;
+    return UNKNOWN_ELEMENT;
+}
+
 typedef struct {
-	const char *name;
-	const int base_g_power;
+	char name[50];
+	int base_g_power;
 	int g_power;
 	Element element;
 } Bakugan;
 
-Bakugan Create_Bakugan(const char *name, int base_g_power, Element element) 
+Bakugan Create_Bakugan(char *name, int base_g_power, Element element) 
 {
-	Bakugan bakugan = { 
-		name,
-		base_g_power, 
-		base_g_power,
-		element
-	};
+	Bakugan bakugan;
+    strncpy(bakugan.name, name, sizeof(bakugan.name) - 1); 
+    bakugan.name[sizeof(bakugan.name) - 1] = '\0';
+    bakugan.base_g_power = base_g_power;
+    bakugan.g_power = base_g_power;
+    bakugan.element = element;
 
 	return bakugan;
 }
@@ -47,10 +60,55 @@ void display_status(Bakugan bakugan) {
 	);
 }
 
+int load_bakugans(const char *filename, Bakugan *bakugans, int bakugan_count)
+{
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL) 
+	{
+		printf("Error: File not found!");
+		return 0;
+	}
+
+	char line[100];
+	int count = 0;
+
+	while (fgets(line, sizeof(line), file) != NULL && count < bakugan_count)
+	{
+		char element_str[20];
+		int parsed = sscanf(line, "%[^,],%d,%s", bakugans[count].name, &bakugans[count].base_g_power, element_str);
+
+		if (parsed == 3)
+		{
+			bakugans[count].g_power = bakugans[count].base_g_power;
+			bakugans[count].element = string_to_element(element_str);
+			count++;
+		}
+		else 
+		{
+			printf("Error: No correct bakugan paramentrs!")
+			return 0;
+		}
+	}
+
+	fclose(file);
+	return count;
+}
+
 int main()
 {
-	Bakugan drago = Create_Bakugan("Dragonoid", 370, PYRUS);
+	Bakugan collection[10];
+	int total = load_bakugans("bakugans.csv", collection, 10);
 
-	display_status(drago);
+	if (total == 0) return 1;
+
+	printf("Total bakugans: %d\n", total);
+
+	for (int i = 0; i < total; i++)
+	{
+		printf("%d.", i + 1);
+		display_status(collection[i]);
+	}
+
 	return 0;
 }
